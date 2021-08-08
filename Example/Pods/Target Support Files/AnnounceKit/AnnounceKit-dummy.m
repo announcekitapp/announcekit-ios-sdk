885f475385c33ef82a5eec537d6945da47da1dce
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2021-08-08 21:19:02 +0300

    Initialize cocoapods

---
+++ Example/Pods/Target Support Files/AnnounceKit/AnnounceKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AnnounceKit : NSObject
+@end
+@implementation PodsDummy_AnnounceKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-AnnounceKit_Example/Pods-AnnounceKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AnnounceKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AnnounceKit_Example
+@end
