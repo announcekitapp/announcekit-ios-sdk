47c55a069e5e283e3491bf683c70e5d13e2eb793
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2021-08-15 21:38:21 +0300

    Add fullscreen widget support to demo

--- Example/AnnounceKit/ViewController.swift
+++ Example/AnnounceKit/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import AnnounceKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var announceKitView: AnnounceKitView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let settings = AnnounceKitSettings(text: "What's New", widget: "1yAi3u", userID: String(Date().timeIntervalSince1970))
+        announceKitView.delegate = self
+        announceKitView.settings = settings
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+
+        super.viewWillAppear(animated)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+            self.announceKitView.displayContent()
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,3 +37,29 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: AnnounceKitDelegate {
+
+    func announceKitView(_ view: AnnounceKitView, didInitialize widget: String) {
+
+    }
+
+    func announceKitView(_ view: AnnounceKitView, didOpenWidget widget: String) {
+
+    }
+
+    func announceKitView(_ view: AnnounceKitView, didCloseWidget widget: String) {
+
+    }
+
+    func announceKitView(_ view: AnnounceKitView, didInitializeWidget widget: String) {
+
+    }
+
+    func announceKitView(_ view: AnnounceKitView, didResizeWidget widget: String, size: CGSize) {
+
+    }
+
+    func announceKitView(_ view: AnnounceKitView, didUpdateUnreadCount count: Int, widget: String) {
+
+    }
+}
