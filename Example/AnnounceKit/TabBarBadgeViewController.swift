3797e3c3c8246b53f8a62a5b5b400583bd32d325
Seyfeddin Başsaraç <seyfeddin@wearethread.co>
2021-09-18 15:58:39 +0300

    Add a tab bar badge example

---
+++ Example/AnnounceKit/TabBarBadgeViewController.swift
@@ -0,0 +1,59 @@
+//
+//  TabBarBadgeViewController.swift
+//  AnnounceKit_Example
+//
+//  Created by Seyfeddin Bassarac on 18.09.2021.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import AnnounceKit
+
+class TabBarBadgeViewController: UIViewController {
+
+    private var announceKitClient: AnnounceKitClient!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let settings = AnnounceKitSettings(widget: "3xdhio")
+        announceKitClient = AnnounceKitClient(withSettings: settings, viewControllerToPresent: self)
+        announceKitClient.delegate = self
+        announceKitClient.startWidget()
+    }
+}
+
+// MARK: - AnnounceKitDelegate
+
+extension TabBarBadgeViewController: AnnounceKitDelegate {
+
+    func announceKitView(
+        _ client: AnnounceKitClient,
+        didInitialize widget: String
+    ) {}
+
+    func announceKitView(
+        _ client: AnnounceKitClient,
+        didOpenWidget widget: String
+    ) {}
+
+    func announceKitView(
+        _ client: AnnounceKitClient,
+        didCloseWidget widget: String
+    ) {}
+
+    func announceKitView(
+        _ client: AnnounceKitClient,
+        didInitializeWidget widget: String
+    ) {}
+
+    func announceKitView(
+        _ client: AnnounceKitClient,
+        didUpdateUnreadCount count: Int,
+        widget: String
+    ) {
+
+        tabBarItem.badgeValue = String(count)
+        tabBarItem.badgeColor = .systemRed
+    }
+}
